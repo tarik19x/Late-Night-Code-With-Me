d942e947dc64ec313905df1a5a0928dd5ee7d3de
Asus <tarikhasan12200@gmail.com>
2020-06-17 12:49:21 +0600

    deleting some files

--- a.py
+++ a.py
@@ -1,6 +1,6 @@
 #2 months on SL! Thank ya'll!
 import random
-first = ("Super", "Raider", "Great", "Rise", "Automata", "Sacrifice", "Hunter ", "Hunter World", "Shadow ", "Monster Hunter ", "Devil ", "Gummy", "Yummy", "Masked", "Unusual", "American", "Bisexual", "MLG", "Mlg", "lil", "Lil")
+first = ("Super", "Raider", "Great", "Rise", "Automata", "Sacrifice", "Hunter ", "Hunter World", "Shadow ", "Monster Hunter ", "Devil tarik19x/Late-Night-Code-With-Metarik19x/Late-Night-Code-With-Metarik19x/Late-Night-Code-With-Me", "Gummy", "Yummy", "Masked", "Unusual", "American", "Bisexual", "MLG", "Mlg", "lil", "Lil")
 second = ("Coder", "Vegan", "Man", "Hacker", "Horse", "Bear", "Goat", "Goblin", "Learner", "Killer", "Woman", "Programmer", "Spy", "Stalker", "Spooderman", "Carrot", "Goat", "Quickscoper", "Quickscoper")
 firrst = random.choice(first)
 seccond = random.choice(second)
--- copycopy.py
+++
@@ -1,5 +0,0 @@
-test_case=int(input())
-for i in range(test_case):
-    n=input()
-    s=set([int(i) for i in input().split()])
-    print(len(s))
